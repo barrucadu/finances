c7671f39ba98091fafd3f690d4596cad4c445bd8
Michael Walker <mike@barrucadu.co.uk>
2017-07-18 01:58:41 +0100

    Move account name logic to a configuration file

--- Main.hs
+++ Main.hs
@@ -1,32 +1,108 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import           Control.Applicative           ((*>), (<|>))
 import           Control.Arrow                 (second)
 import           Control.Monad                 (join, mapM)
 import           Data.Aeson                    (Value, object, (.=))
 import qualified Data.Aeson                    as A
+import qualified Data.Aeson.Types              as A
 import           Data.Char                     (toUpper)
+import           Data.Foldable                 (toList)
+import qualified Data.HashMap.Lazy             as HM
 import           Data.List                     (inits, mapAccumL, nub)
 import qualified Data.Map                      as M
-import           Data.Maybe                    (fromMaybe, maybeToList)
+import           Data.Maybe                    (fromMaybe, listToMaybe,
+                                                mapMaybe, maybeToList)
 import           Data.Ratio                    (Rational)
 import           Data.Semigroup                ((<>))
 import qualified Data.Text                     as T
 import qualified Data.Time.Calendar            as C
 import qualified Data.Time.Clock               as C
 import qualified Data.Time.Format              as C
+import qualified Data.Yaml                     as Y
 import           Hledger.Data.Types            as H
 import           Hledger.Read                  as H
-import qualified Network.HTTP.Types.URI        as W
 import qualified Network.HTTP.Types.Status     as W
+import qualified Network.HTTP.Types.URI        as W
 import qualified Network.Wai                   as W
 import qualified Network.Wai.Handler.Warp      as W
 import qualified Network.Wai.Middleware.Static as W
+import           System.Exit                   (exitFailure)
+import           System.FilePath               (FilePath, (</>))
 import           Text.Read                     (readMaybe)
 
+
 main :: IO ()
-main = W.runEnv 5000 $ serveStatic (\req respond -> respond =<< serveDynamic req) where
-  serveStatic = W.staticPolicy $ W.only [("", "static/index.html")] W.<|> W.addBase "static"
+main = do
+  config <- Y.decodeFileEither "config.yaml"
+  case config of
+    Right cfg -> run cfg
+    Left err  -> do
+      putStrLn "Couldn't parse configuration file:"
+      putStrLn (Y.prettyPrintParseException err)
+      exitFailure
+
+
+-------------------------------------------------------------------------------
+
+-- | The parsed configuration file.
+data Config = Config
+  { port :: Int
+  , staticdir :: FilePath
+  , assetRules :: AccountRules
+  , incomeRules :: AccountRules
+  , budgetRules :: AccountRules
+  , expenseRules :: AccountRules
+  } deriving Show
+
+instance Y.FromJSON Config where
+  parseJSON (Y.Object o) = do
+    Y.Object httpcfg <- o Y..: "http"
+    Y.Object acccfg  <- o Y..: "accounts"
+    Config
+      <$> httpcfg Y..: "port"
+      <*> httpcfg Y..: "static_dir"
+      <*> (A.parseJSON =<< acccfg Y..: "assets")
+      <*> (A.parseJSON =<< acccfg Y..: "income")
+      <*> (A.parseJSON =<< acccfg Y..: "budget")
+      <*> (A.parseJSON =<< acccfg Y..: "expenses")
+  parseJSON x = A.typeMismatch "config" x
+
+-- | Rules for an account name
+data AccountRules
+  = Only [(T.Text, T.Text)]
+  -- ^ A fixed list of transformations.
+  | Try [AccountRules]
+  -- ^ Try the rules in order.
+  | Simple T.Text
+  -- ^ Use the simple rule (drop prefix + capitalise words)
+  | None
+  -- ^ Forbid everything.
+  deriving Show
+
+instance Y.FromJSON AccountRules where
+  parseJSON (Y.Object o) = only <|> try <|> simple <|> none where
+    only = o Y..: "only" >>= \case
+      Y.Object o_ -> Only . HM.toList <$> mapM A.parseJSON o_
+      x -> A.typeMismatch "only" x
+    try = o Y..: "try" >>= \case
+      Y.Array v_ -> Try . toList <$> mapM A.parseJSON v_
+      x -> A.typeMismatch "try" x
+    simple = o Y..: "simple" >>= \case
+      Y.String s_ -> pure (Simple s_)
+      x -> A.typeMismatch "simple" x
+    none = (o Y..: "none" :: A.Parser A.Value) *> pure None
+  parseJSON x = A.typeMismatch "account rules" x
+
+
+-------------------------------------------------------------------------------
+
+-- | Run the web server.
+run :: Config -> IO ()
+run cfg = W.runEnv (port cfg) $ serveStatic (\req respond -> respond =<< serveDynamic req) where
+  serveStatic = W.staticPolicy $ W.only [("", staticdir cfg </> "index.html")] W.<|> W.addBase (staticdir cfg)
   serveDynamic req = fmap (W.responseLBS W.ok200 [] . A.encode) $ do
     today <- C.utctDay <$> C.getCurrentTime
     let qstr = map (second (fmap T.unpack)) . W.queryToQueryText . W.queryString $ req
@@ -37,22 +113,22 @@ main = W.runEnv 5000 $ serveStatic (\req respond -> respond =<< serveDynamic req
       Just m ->
         let y = fromMaybe ((\(y,_,_) -> y) (C.toGregorian today)) qyear
             d = fromMaybe (C.gregorianMonthLength y m) qday
-        in financeDataFor (C.fromGregorian y m d)
-      _ -> financeDataFor today
+        in financeDataFor cfg (C.fromGregorian y m d)
+      _ -> financeDataFor cfg today
 
 -- | Get the data from the default hledger journal for the given date.
-financeDataFor :: C.Day -> IO Value
-financeDataFor today =
-  dataFor today . H.jtxns <$> H.defaultJournal
+financeDataFor :: Config -> C.Day -> IO Value
+financeDataFor cfg today =
+  dataFor cfg today . H.jtxns <$> H.defaultJournal
 
 -- | Get the data for a specific day.
-dataFor :: C.Day -> [H.Transaction] -> Value
-dataFor today txns = object
+dataFor :: Config -> C.Day -> [H.Transaction] -> Value
+dataFor cfg today txns = object
     [ "when"     .= T.pack (C.formatTime C.defaultTimeLocale "%B %_Y" today)
-    , "assets"   .= balanceFrom (const epoch) assetAccount   id
-    , "income"   .= balanceFrom monthStart    incomeAccount  negate
-    , "budget"   .= balanceFrom (const epoch) budgetAccount  id
-    , "expenses" .= balanceFrom monthStart    expenseAccount id
+    , "assets"   .= balanceFrom (const epoch) (account (assetRules   cfg)) id
+    , "income"   .= balanceFrom monthStart    (account (incomeRules  cfg)) negate
+    , "budget"   .= balanceFrom (const epoch) (account (budgetRules  cfg)) id
+    , "expenses" .= balanceFrom monthStart    (account (expenseRules cfg)) id
     , "history"  .= history
     ]
   where
@@ -143,33 +219,13 @@ monthStart d = C.addDays (-1) (let (m, y) = monthYear d in C.fromGregorian y m 1
 toDouble :: Rational -> Double
 toDouble = fromRational
 
--------------------------------------------------------------------------------
-
--- | Give a nice name to an asset account.
-assetAccount :: T.Text -> Maybe T.Text
-assetAccount "assets:cavendish"  = Just "Cavendish Online"
-assetAccount "assets:nationwide" = Just "Nationwide"
-assetAccount "assets:santander:esaver" = Just "Santander (eSaver)"
-assetAccount "assets:santander:main"   = Just "Santander (main)"
-assetAccount _ = Nothing
-
--- | Give a nice name to an income account.
-incomeAccount :: T.Text -> Maybe T.Text
-incomeAccount "income:pta" = Just "PTA"
-incomeAccount acc = simpleAccountName "income" acc
-
--- | Give a nice name to a budget account.
-budgetAccount :: T.Text -> Maybe T.Text
-budgetAccount = simpleAccountName "assets:santander:main:budget"
-
--- | Give a nice name to an expense account.
-expenseAccount :: T.Text -> Maybe T.Text
-expenseAccount = simpleAccountName "expenses"
-
--- | Check for a prefix and, if found, remove it and capitalise all
--- words.
-simpleAccountName :: T.Text -> T.Text -> Maybe T.Text
-simpleAccountName prefix acc = do
-  name  <- T.stripPrefix (prefix <> ":") acc
-  words <- mapM T.uncons (T.words name)
-  pure . T.unwords $ map (\(c, rest) -> T.cons (toUpper c) rest) words
+-- | Give a nice name to an account.
+account :: AccountRules -> T.Text -> Maybe T.Text
+account rules0 acc = go rules0 where
+  go (Only whitelist) = lookup acc whitelist
+  go (Try rules) = listToMaybe (mapMaybe go rules)
+  go (Simple prefix)  = do
+    name  <- T.stripPrefix (prefix <> ":") acc
+    words <- mapM T.uncons (T.words name)
+    pure . T.unwords $ map (\(c, rest) -> T.cons (toUpper c) rest) words
+  go None = Nothing
